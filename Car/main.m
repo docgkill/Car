94252dfc561bc70102049c460923790e88e5d95a
docgkill <docgkill@gmail.com>
2012-09-13 23:23:10 +0900

    Initial Commit

---
+++ Car/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Car
+//
+//  Created by ktds on 12. 9. 13..
+//  Copyright (c) 2012년 godev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
