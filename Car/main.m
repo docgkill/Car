85d452ae032cd94c8f970a2e3665f6cd738e14a9
docgkill <docgkill@gmail.com>
2012-09-14 00:34:08 +0900

    .

--- Car/main.m
+++ Car/main.m
@@ -14,6 +14,7 @@ int main(int argc, const char * argv[])
         printf("sizeof(object) %lu\n", sizeof(object));
         /*
          MRR 컴파일러는 아래 코드가 정상 동작한다. 그러나 기본 컴파일러인 ARC 컴파일러에는 동작하지 않는다.
+         그래서 class_getInstanceSize를 사용하며 이를 사용하기 위해 objc-runtime.h을 사용한다.
          MRR은 manual retain/release 컴파일러이며 , ARC는 자동화된 레퍼런스 카운팅 체계를 가진 컴파일러이다.
          */
 //        printf("sizeof(*object) %lu\n", sizeof(*object));
